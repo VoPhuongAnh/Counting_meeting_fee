2950789f303c10ac785fd2f7b95e32ab97e32628
Vo Phuong Anh <chansoo194@gmail.com>
2023-10-24 13:49:44 +0700

    add new file meeting_fee

---
+++ meeting_fee.py
@@ -0,0 +1,14 @@
+# Nhập thời gian start booking
+start_time = input("Enter Starting time:")
+
+# Nhập thời gian end booking:
+end_time = input("Enter Ending time:")
+
+# Nhập thời gian báo cancel booking:
+cancel_time = input()
+
+# Tính thời gian từ lúc start booking tới lúc cancel booking
+
+
+
+
