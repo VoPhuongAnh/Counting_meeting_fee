401b5d60c4b69c26a8e134e49057d187f239e1a0
Vo Phuong Anh <chansoo194@gmail.com>
2023-10-26 16:14:24 +0700

    update 2 files

--- meeting_fee.py
+++ meeting_fee.py
@@ -1,14 +1,45 @@
+from datetime import datetime, date
+
 # Nhập thời gian start booking
-start_time = input("Enter Starting time:")
+
+t2 = datetime(year = 2023, month = 10, day = 25, hour = 16, minute = 0, second = 0)
 
 # Nhập thời gian end booking:
-end_time = input("Enter Ending time:")
+
+t3 = datetime(year= 2023, month= 10, day= 23, hour= 18, minute= 0, second= 0)
 
 # Nhập thời gian báo cancel booking:
-cancel_time = input()
-
-# Tính thời gian từ lúc start booking tới lúc cancel booking
-
-print(start_time)
+t1 = datetime(year= 2023, month= 10, day= 24, hour= 11, minute= 40, second= 0)
 
 
+print('Enter Start time (yyyy-mm-dd hour:minute:second): ')
+print('start time: ', t2)
+print('end time: ', t3)
+print('cancelled time: ', t1)
+
+# Tính thời gian từ lúc start booking tới lúc cancel booking dùng timedelta
+
+t4 = t2 - t1
+print(t4)
+
+# Convert thời gian chênh lệch về ngày:
+
+t = t4.total_seconds() / 3600
+
+# Convert về ngày:
+
+print('Cancel booking within: ', t)
+
+# Tính phần trăm penalty:
+
+def percent_charge(t):
+    # while t > 0:
+        if (t > 48):
+            charge = 0
+        elif t > 24:
+            charge = 50
+        else:
+            charge = 100
+        return charge
+
+print('Phần trăm Booking fee phải thanh toán: ', percent_charge(t), '%')
